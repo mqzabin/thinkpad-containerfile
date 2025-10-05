9d253ca2db5362953fa16f532ffdb50f43c67fbe
Matheus Zabin <matheusqueirozzabin@gmail.com>
2025-10-05 17:28:25 -0300

    Initial setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,13 +4,17 @@ set -ouex pipefail
 
 ### Install packages
 
+# Kernel builds could be found here:
+# https://bodhi.fedoraproject.org/updates/?packages=kernel&page=1
+rpm-ostree override replace https://koji.fedoraproject.org/koji/buildinfo?buildID=2693823
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
 
 # Use a COPR Example:
 #
@@ -21,4 +25,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
