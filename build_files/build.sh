3ce17b3128f91bd434ca3ffb18f527822e74026f
Matheus Zabin <matheusqueirozzabin@gmail.com>
2025-10-05 22:27:55 -0300

    fix: remove 1password

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,26 +29,3 @@ sudo dnf -q makecache -y --disablerepo="*" --enablerepo="firefoxpwa"
 # Install the package
 sudo dnf install -y firefoxpwa
 
-########################
-# Installing 1Password #
-########################
-# See:
-# - https://support.1password.com/install-linux/#fedora-or-red-hat-enterprise-linux
-
-# Add the key for the 1Password yum repository
-sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
-
-# Add the 1Password yum repository:
-sudo tee /etc/yum.repos.d/1password.repo > /dev/null << EOF
-[1password]
-name=1Password Stable Channel
-baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://downloads.1password.com/linux/keys/1password.asc
-EOF
-
-# Install 1Password:
-sudo dnf install -y 1password
-
