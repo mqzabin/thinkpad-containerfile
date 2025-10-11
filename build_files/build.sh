d2f34891e5e8253cb9e28c4e6d71d9bc767729ee
Matheus Zabin <matheusqueirozzabin@gmail.com>
2025-10-11 17:51:28 -0300

    fix: install fingerprint

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,30 +2,10 @@
 
 set -ouex pipefail
 
+# Install packages
 
-####################################
-# Installing Firefox PWA connector #
-####################################
-# See:
-# - https://packagecloud.io/filips/FirefoxPWA
-# - https://addons.mozilla.org/pt-BR/firefox/addon/pwas-for-firefox/
+# Fingerprint setup, following:
+# https://www.reddit.com/r/Fedora/comments/1gj5jbn/get_fingerprint_authentication_working_fedora_41/
 
-# Enable the repository
-sudo tee /etc/yum.repos.d/firefoxpwa.repo > /dev/null <<EOF
-[firefoxpwa]
-name=FirefoxPWA
-metadata_expire=7d
-baseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch
-gpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey
-       https://packagecloud.io/filips/FirefoxPWA/gpgkey/filips-FirefoxPWA-912AD9BE47FEB404.pub.gpg
-repo_gpgcheck=1
-gpgcheck=1
-enabled=1
-EOF
-
-# Import GPG key and update DNF cache
-sudo dnf -q makecache -y --disablerepo="*" --enablerepo="firefoxpwa"
-
-# Install the package
-sudo dnf install -y firefoxpwa
+sudo dnf reinstall -y fprintd libfprint
 
