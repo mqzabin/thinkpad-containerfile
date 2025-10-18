a50f2b010104838e79a14be4274e58ce2605802a
Matheus Zabin <matheusqueirozzabin@gmail.com>
2025-10-17 21:11:53 -0300

    feat: add rpm fusion

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,19 @@
 
 set -ouex pipefail
 
-# Install packages
+# Enabling RPM Fusion repos:
+rpm-ostree install -y \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+rpm-ostree override remove \
+  libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free ffmpeg-free libavdevice-free \
+  --install ffmpeg
 
 # Fingerprint setup, following:
 # https://www.reddit.com/r/Fedora/comments/1gj5jbn/get_fingerprint_authentication_working_fedora_41/
 
-sudo dnf reinstall -y fprintd libfprint
+rpm-ostree install -y fprintd libfprint
+
+rpm-ostree install -y libva-intel-media-driver
 
